134c4dbcf346013569aec44094f2eb42c2ecd1bd
HubertSienicki <businesssienicki@gmail.com>
2022-11-24 16:26:02 +0100

    Finished basic html for CLI

--- src/js/TerminalEmulator/TerminalEmulator.js
+++ src/js/TerminalEmulator/TerminalEmulator.js
@@ -1,19 +1,32 @@
 export class TerminalEmulator {
 	constructor() {
 		this.defaultText = "visitor@user:~$";
-		this.input = "";
+		this.inputText = "";
+		this.outputText = "";
 	}
 
 	init() {
-		document.getElementById("text").innerHTML = this.defaultText;
+		document.getElementById("terminal_emulator_input_input_text").innerHTML =
+			this.defaultText;
 		document.getElementById("inputField").focus();
 		document.addEventListener("keypress", (e) => {
-			var keyName = e.key;
 			var keyCode = e.code;
 
 			if (keyCode == "Enter") {
 				console.log("Enter pressed!");
+				this.inputText =
+					this.defaultText + document.getElementById("inputField").value;
+				this.setOutputText(this.inputText);
 			}
 		});
+	}
+
+	setOutputText(outputText) {
+		this.outputText = outputText + "<br>" + "command here";
+
+		document.getElementById("terminal_emulator_output").innerHTML +=
+			this.outputText + "<br><br>";
+
+		document.getElementById("inputField").value = "";
 	}
 }
